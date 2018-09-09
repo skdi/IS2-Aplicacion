01ce674ba3b94d9342f8da2bf2f7dfcea4b81a39
Nicolas <nicojiart@gmail.com>
2018-09-09 15:14:41 -0500

    Barra de estado
    primer avance con la barra de estado

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,minimum-scale=1">
+	<title>Proyecto IS2</title>
+	<link rel="stylesheet" href="css\estilos.css">
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+</head>
+<body>
+<div class="sidebar">
+	<h2>MENU</h2>
+	<ul>
+		<li><a href="">Inicio</a></li>
+		<li><a href="">Iniciar Sesión</a></li>
+		<li><a href="">Registrarse</a></li>
+		<li><a href="">Contactenos</a></li>
+        <li><a href="">Siguenos</a></li>
+	</ul>
+</div>
+<div class="contenido">
+	<img src="imag\menu.png" alt="" class="menu-bar"> 
+</div>
+	 <script src="js\abrir.js"></script>
+</body>
+</html>
